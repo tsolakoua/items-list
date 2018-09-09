2ed935a85d92cbdb33f6f239ec780373f17ec1fd
Anna Tsolakou <tsolakou.a@gmail.com>
2018-09-09 15:05:50 +0200

    add view

--- script.js
+++ script.js
@@ -1,37 +1,20 @@
 var itemsList = {
     items: [],
-    displayItems: function() {
-        console.log('My items');
-        if (this.items.length === 0) {
-            console.log('The list is empty');
-        } else {}
-        for (var i = 0; i < this.items.length; i++) {
-            if (this.items[i].completed === false) {
-                console.log('( )', this.items[i].itemName);
-            } else {
-                console.log('(x)', this.items[i].itemName);
-            }
-        }
-    },
     addItem: function(itemName) {
         this.items.push({
             itemName: itemName,
             completed: false
         });
-        this.displayItems();
     },
     deleteItem: function(index) {
         this.items.splice(index, 1);
-        this.displayItems();
     },
     changeItem: function(index, itemName) {
         this.items[index].itemName = itemName;
-        this.displayItems();
     },
     toggleCompleted: function(index) {
         var toggle_status = this.items[index].completed;
         this.items[index].completed = !toggle_status;
-        this.displayItems();
     },
     toggleAll: function() {
         var count = 0;
@@ -49,32 +32,51 @@ var itemsList = {
                 this.items[i].completed = false;
             }
         }
-        this.displayItems();
     }
 };
 
 var handlers = {
-  displayItems: function() {
-    itemsList.displayItems();
-  },
   toggleAll: function() {
     itemsList.toggleAll();
+    view.displayItems();
   },
   addItem: function() { 
     itemsList.addItem(document.getElementById('addItemText').value);
     document.getElementById('addItemText').value = '';
+    view.displayItems();    
   },  
   changeItem: function() { 
     itemsList.changeItem(document.getElementById('addPosition').value,document.getElementById('updatedItemText').value );
     document.getElementById('addPosition').value = '';
     document.getElementById('updatedItemText').value = '';
+    view.displayItems();    
   },
   deleteItem: function() { 
     itemsList.deleteItem(document.getElementById('deletePosition').value);
     document.getElementById('deletePosition').value = '';
+    view.displayItems();    
   },
   toggleCompleted: function() { 
     itemsList.toggleCompleted(document.getElementById('togglePosition').value);
     document.getElementById('togglePosition').value = '';
+    view.displayItems();
   }
+};
+
+var view = {
+  displayItems: function() {
+    var itemsUl = document.querySelector('ul');
+    itemsUl.innerHTML = '';
+    for (var i = 0; i < itemsList.items.length; i++) {
+      var itemsLi = document.createElement('li');
+      var textCompleted = '';
+      if (itemsList.items[i].completed === true) {
+        textCompleted = '(x) ';
+      } else {
+        textCompleted = '( ) ';
+      }
+      itemsLi.textContent = textCompleted + itemsList.items[i].itemName;
+      itemsUl.appendChild(itemsLi);
+    }
+  }  
 };
