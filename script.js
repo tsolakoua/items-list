4868bd5288787b5ade29209f0cdd556e2ee462e3
Anna Tsolakou <tsolakou.a@gmail.com>
2018-09-17 11:37:24 +0200

    toggle all final

--- script.js
+++ script.js
@@ -18,100 +18,83 @@ var itemsList = {
     },
     toggleAll: function() {
         var count = 0;
-       /* for (var i = 0; i < this.items.length; i++) {
-            if (this.items[i].completed === false) {
+        totalItems = this.items.length;
+
+        this.items.forEach(function(item) {
+            if (item.completed === false) {
                 count++;
             }
-        } */
-        
-        this.items.forEach(function(item){
-          if (item.completed === false) {
-            count++;
-          }
         });
-        
-        
-        if (count === this.items.length) {
-          
-            /* for (var i = 0; i < this.items.length; i++) {
-                this.items[i].completed = true;
-            } */
-            
-            this.items.forEach(function(item){
-              item.completed = true;
-            });
-        } else {
-          
-            /* for (var i = 0; i < this.items.length; i++) {
-                this.items[i].completed = false;
-            } */
-            
-            this.items.forEach(function(item){
-              item.completed = false;
-            });
-        }
+
+        this.items.forEach(function(item) {
+            if (count === totalItems) {
+                item.completed = true;
+            } else {
+                item.completed = false;
+            }
+        });
     }
 };
 
 var handlers = {
-  toggleAll: function() {
-    itemsList.toggleAll();
-    view.displayItems();
-  },
-  addItem: function() { 
-    itemsList.addItem(document.getElementById('addItemText').value);
-    document.getElementById('addItemText').value = '';
-    view.displayItems();    
-  },  
-  changeItem: function() { 
-    itemsList.changeItem(document.getElementById('addPosition').value,document.getElementById('updatedItemText').value );
-    document.getElementById('addPosition').value = '';
-    document.getElementById('updatedItemText').value = '';
-    view.displayItems();    
-  },
-  deleteItem: function(position) { 
-    itemsList.deleteItem(position);
-    view.displayItems();    
-  },
-  toggleCompleted: function() { 
-    itemsList.toggleCompleted(document.getElementById('togglePosition').value);
-    document.getElementById('togglePosition').value = '';
-    view.displayItems();
-  }
+    toggleAll: function() {
+        itemsList.toggleAll();
+        view.displayItems();
+    },
+    addItem: function() {
+        itemsList.addItem(document.getElementById('addItemText').value);
+        document.getElementById('addItemText').value = '';
+        view.displayItems();
+    },
+    changeItem: function() {
+        itemsList.changeItem(document.getElementById('addPosition').value, document.getElementById('updatedItemText').value);
+        document.getElementById('addPosition').value = '';
+        document.getElementById('updatedItemText').value = '';
+        view.displayItems();
+    },
+    deleteItem: function(position) {
+        itemsList.deleteItem(position);
+        view.displayItems();
+    },
+    toggleCompleted: function() {
+        itemsList.toggleCompleted(document.getElementById('togglePosition').value);
+        document.getElementById('togglePosition').value = '';
+        view.displayItems();
+    }
 };
 
 var view = {
-  displayItems: function() {
-    var itemsUl = document.querySelector('ul');
-    itemsUl.innerHTML = '';
-    for (var i = 0; i < itemsList.items.length; i++) {
-      var itemsLi = document.createElement('li');
-      var textCompleted = '';
-      if (itemsList.items[i].completed === true) {
-        textCompleted = '(x) ';
-      } else {
-        textCompleted = '( ) ';
-      }
-      itemsLi.textContent = textCompleted + itemsList.items[i].itemName;
-      itemsLi.appendChild(this.createDeleteButton());
-      itemsUl.appendChild(itemsLi);
+    displayItems: function() {
+        var itemsUl = document.querySelector('ul');
+        itemsUl.innerHTML = '';
+        for (var i = 0; i < itemsList.items.length; i++) {
+            var itemsLi = document.createElement('li');
+            var textCompleted = '';
+            if (itemsList.items[i].completed === true) {
+                textCompleted = '(x) ';
+            } else {
+                textCompleted = '( ) ';
+            }
+            itemsLi.textContent = textCompleted + itemsList.items[i].itemName;
+            itemsLi.appendChild(this.createDeleteButton());
+            itemsUl.appendChild(itemsLi);
+        }
+    },
+    createDeleteButton: function() {
+        var deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.className = 'deleteButton';
+        return deleteButton;
+    },
+    setUpEventListeners: function() {
+        var itemsUl = document.querySelector('ul');
+        itemsUl.addEventListener('click', function(event) {
+            var elementClicked = event.target;
+            if (elementClicked.className === 'deleteButton') {
+                handlers.deleteItem(parseInt(elementClicked.parentNode.id));
+            }
+        });
     }
-  },
-  createDeleteButton: function() {
-    var deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.className = 'deleteButton';
-    return deleteButton;
-  },
-  setUpEventListeners: function() {
-    var itemsUl = document.querySelector('ul');
-    itemsUl.addEventListener('click', function(event){
-      var elementClicked = event.target;
-      if (elementClicked.className === 'deleteButton') {
-        handlers.deleteItem(parseInt(elementClicked.parentNode.id));
-      }
-    });
-  }
 };
 
 view.setUpEventListeners();
